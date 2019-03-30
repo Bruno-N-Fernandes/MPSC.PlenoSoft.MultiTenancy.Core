a11957e5c81626cec0714270e9a5dd4f2c7c4873
Bruno N Fernandes <bruno.tere@gmail.com>
2019-03-30 00:54:14 -0300

    Ajustes nos testes

--- src/Tests/Dominio/TestandoUsuario.cs
+++ src/Tests/Dominio/TestandoUsuario.cs
@@ -11,10 +11,10 @@ namespace MPSC.PlenoSoft.MultiTenancy.Core.Testes
 
 		private static ContainerObject Setup(ContainerObject co)
 		{
-			var usuario = co.Adicionar(new Usuario() { Id = 1, Owner = null, Nome = "Bruno Fernandes (Admin)", EMail = "bruno@dominio.com.br", Celular = "21 9 8765-4321" });
+			var usuario = co.Adicionar(new Usuario() { Id = 1, Owner = null, Nome = "Nome Usuario Sobrenome (Admin)", EMail = "usuario@dominio.com.br", Celular = "21 9 8765-4321" });
 			usuario.DefinirSenhaInicial("Abc@123!", "Abc@123!");
 
-			var sistema = co.Adicionar(new Sistema() { Id = 1, Nome = "MPSC.PlenoSoft.MultiTenancy.Core", Sigla = "MT.Core" });
+			var sistema = co.Adicionar(new Sistema() { Id = 1, Nome = "MPSC.PlenoSoft.MultiTenancy.Core", Sigla = "MPSC.PlenoSoft.MultiTenancy.Core" });
 			var organizacao = co.Adicionar(new Organizacao { Id = 1, Titular = usuario, RazaoSocial = "Mercado Pleno Soluções em Computação Ltda M.E." });
 
 			return co;
